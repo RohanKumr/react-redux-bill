0c18d3178ba7b5e56bf14a94f32734f0968a994b
Rohan Kumar <rkrohanrk065@gmail.com>
2020-09-14 02:34:13 +0530

    Updates

---
+++ precache-manifest.e9841265cb55160b64bc77e2258d9426.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa0ee39b801de63da92662760ff7215",
+    "url": "/react-redux-bill/index.html"
+  },
+  {
+    "revision": "a2c86a09da87184bea4b",
+    "url": "/react-redux-bill/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5632a2d1469643f9d0be",
+    "url": "/react-redux-bill/static/css/main.cbfbb130.chunk.css"
+  },
+  {
+    "revision": "a2c86a09da87184bea4b",
+    "url": "/react-redux-bill/static/js/2.bc13c6d1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-redux-bill/static/js/2.bc13c6d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5632a2d1469643f9d0be",
+    "url": "/react-redux-bill/static/js/main.968a6198.chunk.js"
+  },
+  {
+    "revision": "48412aa54bd8f8abee7a",
+    "url": "/react-redux-bill/static/js/runtime-main.f45c8ff4.js"
+  }
+]);
